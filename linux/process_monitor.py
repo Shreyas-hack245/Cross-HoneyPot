73147857379345395c49c10ce328215078150e87
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-06-20 05:16:52 -0400

    changed sleep time to 0.1

--- linux/process_monitor.py
+++ linux/process_monitor.py
@@ -1,5 +1,18 @@
 import psutil
 import time
+from shared.logger import log_event
+
+SUSPICIOUS_TOOLS = [
+    "nmap",
+    "hydra",
+    "john",
+    "hashcat",
+    "sqlmap",
+    "wireshark",
+    "aircrack-ng"
+]
+
+detected = set()
 
 def start_process_monitor():
 
@@ -8,9 +21,32 @@ def start_process_monitor():
     while True:
 
         for proc in psutil.process_iter(['pid', 'name']):
+
             try:
-                print(proc.info['name'])
-            except:
+                name = proc.info['name']
+
+                if not name:
+                    continue
+
+                name = name.lower()
+
+                if name in SUSPICIOUS_TOOLS:
+
+                    unique_id = f"{name}_{proc.info['pid']}"
+
+                    if unique_id not in detected:
+
+                        detected.add(unique_id)
+
+                        alert = (
+                            f"[ALERT] Suspicious Process: "
+                            f"{name} PID={proc.info['pid']}"
+                        )
+
+                        print(alert)
+                        log_event(alert)
+
+            except Exception:
                 pass
 
-        time.sleep(5)
+        time.sleep(0.1)
